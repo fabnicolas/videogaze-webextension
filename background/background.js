3a414daef0cb66df90795cec526eb3d84f84d172
Finalgalaxy <finalgalaxy@gmail.com>
2019-02-14 23:26:24 +0100

    Moved videogaze initialization to background.js.

--- background/background.js
+++ background/background.js
@@ -5,17 +5,33 @@
   var tabs = {}
   var video_tabs = {};
 
+  var inject_videogaze_once = function(callback) {
+    chrome_get_active_tab(actual_tab => {
+      if(!video_tabs[actual_tab.id]) inject_videogaze(callback);
+    });
+  }
+
+  var inject_videogaze = function(callback) {
+    chrome_get_active_tab(actual_tab => {
+      chrome_tabs_executeScripts(
+        actual_tab.id,
+        ['/js/chrome/base.js', '/cs/cs.js', '/js/utils.js', '/global.js', '/room.js'],
+        function() {callback();}
+      );
+    });
+  }
+
   // When extension loads, map inside tabs all URLs of all opened tabs
-  chrome.tabs.getAllInWindow(null, function(all_tabs){
-    for(var i=0; i<all_tabs.length; i++){
-      tabs[all_tabs[i].id]=all_tabs[i].url;             
+  chrome.tabs.getAllInWindow(null, function(all_tabs) {
+    for(var i = 0;i < all_tabs.length;i++) {
+      tabs[all_tabs[i].id] = all_tabs[i].url;
     }
   });
 
   // When an URL changes, reflect the change into tabs object
   chrome.tabs.onUpdated.addListener(function(tab_id, change_info, tab) {
     if(change_info.status == "loading" && change_info.url) {
-      tabs[tab_id]=change_info.url;
+      tabs[tab_id] = change_info.url;
     }
   });
 
@@ -24,8 +40,12 @@
     if(message.init) port_popup.postMessage(message);
 
     // Tell CS to perform room initialization
-    if(message.action == "room" && port_cs != null)
-      port_cs.postMessage(message);
+    if(message.action == "room"){
+      var action=function(){port_cs.postMessage(message);}
+      if(port_cs==null) inject_videogaze_once(action);
+      else action();
+    }
+      
     if(message.action == "getdata")
       port_popup.postMessage({video_tabs: video_tabs});
   };
@@ -44,15 +64,13 @@
   };
 
 
-  // Initialize ports
+  // Initialize ports. Handle popup script and content script connections.
   chrome.runtime.onConnect.addListener(function(connecting_port) {
     if(connecting_port.name == "popup-port") {
       port_popup = connecting_port;
-      /* Handle popup script connection */
       port_popup.onMessage.addListener(handler_popup_port);
     } else if(connecting_port.name == "cs-port") {
       port_cs = connecting_port;
-      /* Handle content script connection */
       port_cs.onMessage.addListener(handler_cs_port);
     }
   });
--- popup/popup.js
+++ popup/popup.js
@@ -1,67 +1,46 @@
 var _video_tabs = [];
 var popup_port = null;
 
-// Checks for videogaze injection
-var inject_videogaze_once = function(callback) {
-	chrome_get_active_tab(actual_tab => {
-		if(!_video_tabs[actual_tab.id]) inject_videogaze(callback);
-	});
-}
-
-var inject_videogaze = function(callback) {
-	chrome_get_active_tab(actual_tab => {
-		chrome_tabs_executeScripts(
-			actual_tab.id,
-			['/js/chrome/base.js', '/cs/cs.js', '/js/utils.js', '/global.js', '/room.js'],
-			function(){callback();}
-		);
-	});
-}
-
 var on_port_open = function(callback) {
-	if(popup_port == null) {
-		popup_port = chrome.runtime.connect({name: "popup-port"});
-		popup_port.onMessage.addListener(function(message) {
-			if(message.init) callback();
-			if(message.video_tabs){
-				_video_tabs = message.video_tabs;
-				chrome_get_active_tab(actual_tab => {
-					if(_video_tabs[actual_tab.id]){
-						document.getElementById('room_details').innerText = 'Room code = ' + 
-							_video_tabs[actual_tab.id].roomcode;
-					}
-				});
-			}
-		});
-		popup_port.postMessage({init: true});
-	} else {
-		callback();
-	}
+  if(popup_port == null) {
+    popup_port = chrome.runtime.connect({name: "popup-port"});
+    popup_port.onMessage.addListener(function(message) {
+      if(message.init) callback();
+      if(message.video_tabs) {
+        _video_tabs = message.video_tabs;
+        chrome_get_active_tab(actual_tab => {
+          if(_video_tabs[actual_tab.id]) {
+            document.getElementById('room_details').innerText = 'Room code = ' +
+              _video_tabs[actual_tab.id].roomcode;
+          }
+        });
+      }
+    });
+    popup_port.postMessage({init: true});
+  } else {
+    callback();
+  }
 }
 
 
-var message_room = function(message_to_send) {
-	if(message_to_send === undefined) message_to_send = null;
-	on_port_open(function() {
-		if(popup_port != null) popup_port.postMessage(message_to_send);
-		else console.log("Error: port is null in popup.js");
-	});
+var port_message = function(message_to_send) {
+  if(message_to_send === undefined) message_to_send = null;
+  on_port_open(function() {
+    if(popup_port != null) popup_port.postMessage(message_to_send);
+    else console.log("Error: port is null in popup.js");
+  });
 }
 
 var onclick_make_room = function() {
-	inject_videogaze_once(function() {
-		message_room({action: "room", roomcode: null});
-	});
+  port_message({action: "room", roomcode: null});
 }
 
 var onclick_join_room = function() {
-	inject_videogaze_once(function() {
-		message_room({action: "room", roomcode: document.getElementById('text_roomcode').value});
-	});
+  port_message({action: "room", roomcode: document.getElementById('text_roomcode').value});
 }
 
 window.onload = function() {
-	message_room({action: 'getdata'});
+  port_message({action: 'getdata'});
 }
 
 
