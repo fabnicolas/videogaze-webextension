891e9ff52b9cec1f704871ea3e9519800403b595
Finalgalaxy <finalgalaxy@gmail.com>
2019-02-19 21:07:09 +0100

    Removed some console.log messages.

--- cs/cs.js
+++ cs/cs.js
@@ -8,10 +8,8 @@
 	port_cs.onMessage.addListener(function(message) {
 		// If background script sends action "room"
 		if(message.action == "room") {
-			console.log(message.roomcode)
 			make_room(message.roomcode, videoplayer, function(roomcode) {
 				// When room is made, send code back to background script (which communicates it to the popup).
-				console.log(roomcode);
 				port_cs.postMessage({code: roomcode});
 			});
 		}
