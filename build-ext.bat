1e67f66161e7cf2490b74e4e88f6949fd3840154
Finalgalaxy <finalgalaxy@gmail.com>
2019-02-20 15:47:11 +0100

    New tools for building development-stage XPI extensions for Firefox Developer Edition with XPI signature checks disabled.

--- build-ext.bat
+++ build-ext.bat
@@ -1,1 +1,4 @@
-git archive --format=zip HEAD -o videogaze.zip
+@echo off
+@del videogaze-extension.xpi
+@"C:\Program Files\7-Zip\7z" a -xr@.gitignore -xr@.xpiignore package.zip *
+ren package.zip videogaze-extension.xpi
