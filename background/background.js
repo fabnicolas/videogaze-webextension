af0f3269be80baf86ff617158251ba5ccc7ca27d
Finalgalaxy <finalgalaxy@gmail.com>
2019-02-21 23:13:32 +0100

    VideoGaze can now detect new iframes added on runtime and can be injected before video is in DOM.

--- background/background.js
+++ background/background.js
@@ -31,26 +31,40 @@
   var inject_videogaze = function(callback, tab_id) {
     if(tab_id === undefined) tab_id = null;
 
-    var inject_videogaze_func = function(_callback, _tab_id) {
+    // Inject VideoGaze Detector
+    var inject_videogaze_func = function(tab_id) {
       chrome_tabs_executeScripts(
-        _tab_id,
+        tab_id,
         [
           {file: '/js/chrome/storage.js', allFrames: true},
           {file: '/cs/video_detector.js', allFrames: true}
         ],
-        function() {
-          if(video_tabs[_tab_id] === undefined) video_tabs[_tab_id] = {};
-          video_tabs[_tab_id].on_frame_id_ready = _callback;
-        }
-      )
+        function() {inject_videogaze_func_next1(tab_id);}
+      );
     }
 
+    // When VideoGaze Detector is injected, show frames details & store video tab callback
+    var inject_videogaze_func_next1 = function(tab_id) {
+      chrome.webNavigation.getAllFrames({tabId: tab_id}, function(frames) {
+        if(video_tabs[tab_id] === undefined) video_tabs[tab_id] = {};
+        video_tabs[tab_id].frameIds = [];
+        for(var i = 0;i < frames.length;i++) {
+          video_tabs[tab_id].frameIds.push(frames[i].frameId);
+        }
+        console.log(video_tabs);
+      });
+
+      if(video_tabs[tab_id] === undefined) video_tabs[tab_id] = {};
+      video_tabs[tab_id].on_frame_id_with_video_ready = callback;
+    }
+
+    // If tab_id argument is not provided, detect the actual tab ID, else use tab_id value
     if(tab_id == null) {
       chrome_get_active_tab(actual_tab => {
-        inject_videogaze_func(callback, actual_tab.id);
+        inject_videogaze_func(actual_tab.id);
       });
     } else {
-      inject_videogaze_func(callback, tab_id);
+      inject_videogaze_func(tab_id);
     }
   }
 
@@ -66,7 +80,7 @@
    * @param {int} tab_id The tab ID where VideoGaze should be injected.
    * @param {int} frame_id The frame ID, inside a tab, where VideoGaze should be injected.
    */
-  var inject_videogaze_finalize = function(callback, tab_id, frame_id){
+  var inject_videogaze_finalize = function(callback, tab_id, frame_id) {
     chrome_tabs_executeScripts(
       tab_id,
       [
@@ -76,7 +90,7 @@
         {file: '/room.js', frameId: frame_id},
         {file: '/cs/cs.js', frameId: frame_id},
       ],
-      function() {callback();}
+      callback
     );
   }
 
@@ -90,7 +104,7 @@
   // Listen for tabs changes (URL changed, refresh, etc.)
   chrome.tabs.onUpdated.addListener(function(tab_id, change_info, tab) {
     //console.log("TAB_ID=" + tab_id + ", status=" + change_info.status + ", URL changed: " + change_info.url)
-    
+
     // When an URL changes, reflect the change into tabs object
     if(change_info.status == "loading" && change_info.url)
       tabs[tab_id] = change_info.url;
@@ -98,7 +112,6 @@
     // When the page is reloaded: if the tab was a video tab, re-inject VideoGaze again
     if(change_info.status == "complete" && change_info.url === undefined) {
       // VideoGaze tab reloading is actually not supported due to framing-architecture (19/02/2019).
-      delete video_tabs[tab_id];
 
       /*
       if(video_tabs[tab_id]) {
@@ -151,15 +164,39 @@
     }
   };
 
-  var handler_detector_port = function(message) {
-    if(message.video_detected) {
-      chrome_get_active_tab(actual_tab => {
-        inject_videogaze_finalize(
-          video_tabs[actual_tab.id].on_frame_id_ready,
-          actual_tab.id,
-          video_tabs[actual_tab.id].frame_id
+  var handler_detector_port = function(packet) {
+    console.log(packet);
+
+    if(packet.message.new_iframe_found) {
+      chrome.webNavigation.getAllFrames({tabId: packet.tab_id}, function(frames) {
+        frames_vector = [];
+        for(var i = 0;i < frames.length;i++) {
+          frames_vector.push(frames[i].frameId);
+        }
+        var filteredArray = frames_vector.filter(function(x) {
+          return video_tabs[packet.tab_id].frameIds.indexOf(x) < 0;
+        });
+        var new_frame_id = filteredArray[0];
+        chrome_tabs_executeScripts(
+          packet.tab_id,
+          [
+            {file: '/js/chrome/storage.js', frameId: new_frame_id},
+            {file: '/cs/video_detector.js', frameId: new_frame_id}
+          ],
+          function() {inject_videogaze_func_next1(tab_id);}
         );
       });
+
+    }
+    if(packet.message.video_detected) {
+      //if(video_tabs[packet.tab_id] === undefined) video_tabs[packet.tab_id] = {};
+      video_tabs[packet.tab_id].frame_id = packet.frame_id;
+
+      inject_videogaze_finalize(
+        video_tabs[packet.tab_id].on_frame_id_with_video_ready,
+        packet.tab_id,
+        video_tabs[packet.tab_id].frame_id
+      );
     }
   }
 
@@ -180,10 +217,15 @@
       // There can be multiple tabs with multiple frame IDs with VideoGaze detector injected
       var target_tab = connecting_port.sender.tab.id;
       var target_frame = connecting_port.sender.frameId;
-      port_detector[target_tab] = connecting_port;
-      if(video_tabs[target_tab] === undefined) video_tabs[target_tab] = {};
-      video_tabs[target_tab].frame_id = target_frame;
-      port_detector[target_tab].onMessage.addListener(handler_detector_port);
+      if(port_detector[target_tab] === undefined)
+        port_detector[target_tab] = {};
+      port_detector[target_tab][target_frame] = connecting_port;
+      port_detector[target_tab][target_frame].onMessage.addListener(handler_detector_port);
+      port_detector[target_tab][target_frame].postMessage({
+        background_ready: true,
+        tab_id: target_tab,
+        frame_id: target_frame
+      });
     }
   });
 })();
--- cs/video_detector.js
+++ cs/video_detector.js
@@ -2,16 +2,57 @@
   console.log("VideoGaze Detector (video_detector.js) injected into webpage!");
 
   var port_detector = null;
+  var port_data = {};
   var open_port = function(callback) {
     if(port_detector == null) {
       port_detector = chrome.runtime.connect({name: 'port-detector'});
       port_detector.onMessage.addListener(function(message) {
-        if(message.background_ready) callback();
+        if(message.background_ready){
+          port_data = {tab_id: message.tab_id, frame_id: message.frame_id};
+          callback();
+        }
       });
-      port_detector.postMessage({video_detected: true});
+      port_message({init: true});
     } else {
       callback();
     }
+  }
+
+  var port_message = function(message){
+    port_detector.postMessage({...port_data, ...{message: message}})
+  }
+
+  if(window.self === window.top) {
+    // If we are in frame 0 (The current page), let's observe body for new iframes.
+    var mutationObserver = new MutationObserver(function(mutations) {
+      mutations.forEach(function(mutation) {
+        if(mutation.addedNodes && mutation.addedNodes[0] && mutation.addedNodes[0].tagName=="IFRAME"){
+          console.log(mutation.addedNodes[0]);
+          mutation.addedNodes[0].addEventListener('load', function(){
+            open_port(function(){
+              console.log("Sending message new_iframe_found: true");
+              port_message({new_iframe_found: true});
+            })
+          });
+        }
+      });
+    });
+
+    /*var report_iframe = function(){
+      open_port(function(){
+        console.log("Sending message new_iframe_found: true")
+        port_detector.sendMessage({new_iframe_found: true});
+      })
+    }*/
+
+    mutationObserver.observe(document.body, {
+      attributes: false,
+      characterData: false,
+      childList: true,
+      subtree: true,
+      attributeOldValue: false,
+      characterDataOldValue: false
+    });
   }
 
   var detect_video = function(callback) {
@@ -25,10 +66,11 @@
   }
 
   var start_video_detection = function() {
+    console.log("Starting video detection...");
     detect_video(function(videoplayer) {
       var videoplayer_detected = function(_videoplayer) {
         open_port(function() {
-          port_detector.sendMessage({video_detected: true, videoplayer: _videoplayer});
+          port_message({video_detected: true, videoplayer: _videoplayer});
         });
       }
 
@@ -53,12 +95,12 @@
 
 
   if(document.readyState == "ready" || document.readyState == "complete") {
-    clearAllIntervals(function(){
+    clearAllIntervals(function() {
       start_video_detection();
     });
   } else {
     window.onload = function() {
-      clearAllIntervals(function(){
+      clearAllIntervals(function() {
         start_video_detection();
       });
     }
